28d8d52a6be2a079a3073c15c3be4ef464788475
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-01 14:35:07 +0700

    cac phuong thuc

--- src/main/java/com/codegym/service/ProductServiceImpl.java
+++ src/main/java/com/codegym/service/ProductServiceImpl.java
@@ -1,4 +1,48 @@
 package com.codegym.service;
 
-public class ProductServiceImpl {
+import com.codegym.model.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void insertProduct(Product product) throws SQLException {
+
+    }
+
+    @Override
+    public Product selectProduct(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> selectAllProduct() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteProduct(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean updateProduct(Product product) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Product getProductById(int id) {
+        return null;
+    }
+
+    @Override
+    public void insertProductStore(Product product) throws SQLException {
+
+    }
+
+    @Override
+    public List<Product> getProductByName(String name) {
+        return null;
+    }
 }
